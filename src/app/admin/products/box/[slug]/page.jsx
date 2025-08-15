9e11f504da66b47d4a1da4c0621d75147aa2600b
James Ishola <brain@shoutty.app>
2025-08-15 13:13:08 +0400

    no message

---
+++ src/app/admin/products/box/[slug]/page.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// components
+import ProductList from 'src/components/_admin/products/productList';
+import HeaderBreadcrumbs from 'src/components/headerBreadcrumbs';
+
+// api
+import * as api from 'src/services';
+
+// Meta information
+export const metadata = {
+  title: 'Products - Fanboxes',
+  applicationName: 'Fanboxes',
+  authors: 'Fanboxes'
+};
+
+export default async function AdminProducts() {
+  const { data: categories } = await api.getAllCategoriesByAdmin();
+  const { data: brands } = await api.getAllBrandsByAdmin();
+  const { data: shops } = await api.getAllShopsByAdmin();
+
+  return (
+    <>
+      <HeaderBreadcrumbs
+        admin
+        heading="Box Item List"
+        links={[
+          {
+            name: 'Dashboard',
+            href: '/admin'
+          },
+          {
+            name: 'Boxes'
+          }
+        ]}
+        action={{
+          href: `/admin/products/add`,
+          title: 'Add Box Item'
+        }}
+      />
+      <ProductList categories={categories} shops={shops} brands={brands} />
+    </>
+  );
+}
