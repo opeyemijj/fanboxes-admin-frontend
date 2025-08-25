e860e00cfd6cb0ff3642a8a69be7bb1983636117
shahadath-shoutty <a.shahadath@shoutty.app>
2025-08-25 18:58:15 +0600

    made items name max limit 100

--- src/components/forms/boxItem.jsx
+++ src/components/forms/boxItem.jsx
@@ -73,11 +73,11 @@ export default function AddItemForm({ currentProduct, isInitialized = false, isV
     }
   );
   const NewProductSchema = Yup.object().shape({
-    name: Yup.string().required('Item name is required'),
+    name: Yup.string().max(100, 'Name max limit 100 char').required('Item name is required'),
     value: Yup.string().required('Item value is required'),
     weight: Yup.number().max(100, 'Max Limit 100').required('Item weight is required'),
     odd: Yup.number().max(1, 'Max Limit 1').required('Item odd is required'),
-    description: Yup.string().required('Description is required'),
+    description: Yup.string().optional('Description is required'),
     slug: Yup.string().required('Slug is required'),
     images: Yup.array().min(1, 'Image is required')
   });
