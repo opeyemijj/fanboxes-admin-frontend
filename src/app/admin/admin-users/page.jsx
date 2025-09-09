80714978231f2a75ccc6fe4ea6e9fb10c4f4aa83
shahadath-shoutty <a.shahadath@shoutty.app>
2025-09-09 18:42:26 +0600

    bring permission in user admin-user list

--- src/app/admin/admin-users/page.jsx
+++ src/app/admin/admin-users/page.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 // components
 import HeaderBreadcrumbs from 'src/components/headerBreadcrumbs';
 import UsersList from 'src/components/_admin/users/userList';
+import { UsePermissionServer } from 'src/hooks/usePermissionServer';
+import AccessDenied from 'src/components/cards/AccessDenied';
 
 // Meta information
 export const metadata = {
@@ -11,6 +13,14 @@ export const metadata = {
   authors: 'Fanboxes'
 };
 export default function page() {
+  const canView = UsePermissionServer('view_admin_listing'); // check required permission
+
+  if (!canView) {
+    return <AccessDenied message="You are not allowed to manage Admin Users." redirect="/admin/dashboard" />;
+  }
+
+  const canAddAdmin = UsePermissionServer('add_new_admin');
+
   return (
     <>
       <HeaderBreadcrumbs
@@ -25,10 +35,14 @@ export default function page() {
             name: 'Admin Users'
           }
         ]}
-        action={{
-          href: `/admin/admin-users/add`,
-          title: 'Add Admin'
-        }}
+        action={
+          canAddAdmin
+            ? {
+                href: `/admin/admin-users/add`,
+                title: 'Add Admin'
+              }
+            : null
+        }
       />
       <UsersList userType="admin" />
     </>
