0b2956c64b7d6f69893c58cdc91522f914c9be83
shahadath-shoutty <a.shahadath@shoutty.app>
2025-09-17 21:00:08 +0600

    correction from review

--- src/components/table/rows/transection.jsx
+++ src/components/table/rows/transection.jsx
@@ -67,11 +67,17 @@ export default function TransectionRow({ isLoading, row, handleClickOpen, sn }) 
   function formatDateToDDYYYYMM(dateString) {
     const date = new Date(dateString);
 
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
     const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
     const year = date.getFullYear();
 
-    return `${day}/${month}/${year}`;
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12; // Convert to 12-hour format
+
+    return `${day}/${month}/${year} ${hours}:${minutes}${ampm}`;
   }
 
   return (
@@ -85,8 +91,9 @@ export default function TransectionRow({ isLoading, row, handleClickOpen, sn }) 
           <>
             <Label
               sx={{
-                bgcolor: row?.transactionType === 'credit' ? 'success.light' : 'error.light',
-                color: row?.transactionType === 'credit' ? 'success.dark' : 'white'
+                bgcolor: 'white',
+                // bgcolor: row?.transactionType === 'credit' ? 'success.light' : 'error.light',
+                color: row?.transactionType === 'credit' ? 'success.dark' : 'red'
               }}
             >
               {capitalize(row?.transactionType)}
